b7b26dfcea4945d6a2c97adbd3b6887c8d3c4ebe
Per Sandström <per@helloper.com>
2016-04-17 21:28:53 +0200

    Add alarm and refactor things a bit

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Håller dina fingrar borta från snasket…
 
+
+## Credits
+
+Warning sound:  
+["Warning" by erkanozan](https://www.freesound.org/people/erkanozan/sounds/51752/)
--- js/mathem.js
+++ js/mathem.js
@@ -10,89 +10,127 @@
 
   var Mathem = {
 
-    init: function() {
+    Init: function() {
 
       this.Navigation();
-      this.Home();
       this.Products();
+      this.Pages();
 
     },
 
-    blockedURLs: function() {
+    Snask: function() {
       return arr = [
+        "deli/godsaker",
+        "varor/alkoholfritt",
         "varor/amerikansk-lask",
         "varor/bullar",
         "varor/chips",
         "varor/chokladpraliner",
+        "varor/chokladstycksaker",
+        "varor/cider",
+        "varor/drinkmix",
         "varor/energidryck",
         "varor/fardiga-efterratter",
+        "varor/festlig-dricka",
+        "varor/fikabrod-o-tartor",
         "varor/frysta-tartor",
         "varor/ginger-ale",
         "varor/ginger-beer",
         "varor/glass",
+        "varor/glogg",
         "varor/godis",
         "varor/kladdkaka",
         "varor/kondisbitar",
         "varor/lask",
         "varor/lchf-glass",
+        "varor/mazariner",
+        "varor/muffins",
+        "varor/ol",
         "varor/pizza",
         "varor/presentchoklad",
         "varor/rawfood-glass",
         "varor/saft-",
-        "varor/sport--o-energidryck",
-        "varor/vaniljglass"
+        "varor/sport--o-energidryck/",
+        "varor/sport--o-aterhamtn--dryck",
+        "varor/tartor",
+        "varor/tonic",
+        "varor/vaniljglass",
+        "varor/vaniljlangd"
       ];
     },
 
-    blockProducts: function() {
-      var blocked_urls = Mathem.blockedURLs();
+    BlockProducts: function() {
+      var blocked_urls = Mathem.Snask();
 
-      $('.product.prod-info').each(function( index ) {
+      $('.product.prod-info:not(.snaskblock)').each(function( index ) {
         var product = $(this);
         var product_url = $('.prodImg a', $(this)).attr('href');
+        $(this).addClass('snaskblock');
 
         if (typeof product_url === 'string') {
           $.each( blocked_urls, function( i, val ) {
             if (product_url.includes(val)) {
               product.remove();
-              console.log('Snaskblock: ' + product_url);
             }
           });
         }
       });
     },
 
-    Navigation: function() {
-      $('ul.navigation li, .megaContainer ul li').each(function(i) {
-        if ($('a', $(this)).attr('href') == '/varor/glass-godis-o-snacks') {
-          $(this).remove();
-        } else if ($('a', $(this)).attr('href') == '/deli/godsaker') {
-          $(this).remove();
+    BlockNavigation: function() {
+      var blocked_urls = Mathem.Snask();
+
+      $('ul.navigation li, .megaContainer ul li').each(function( index ) {
+        var nav = $(this);
+        var nav_url = $('a', $(this)).attr('href');
+
+        if (typeof nav_url === 'string') {
+          $.each( blocked_urls, function( i, val ) {
+            if (nav_url.includes(val)) {
+              nav.remove();
+            }
+          });
         }
       });
     },
 
-    Home: function() {
-      if (window.location.href == 'https://www.mathem.se/') {
-        setInterval(function(){ Mathem.blockProducts(); }, 1000);
-      }
+    SoundAlarm: function() {
+      $('body').prepend('<div class="snaskblock-warning" style="height: ' + $( document ).height() + 'px"><audio controls="controls" autoplay="autoplay" loop="true"><source src="' + chrome.extension.getURL('audio/warning.wav') + '" type="audio/mpeg"></audio></div>');
+    },
+
+    Navigation: function() {
+      setInterval(function(){ Mathem.BlockNavigation(); }, 1000);
     },
 
     Products: function() {
       if (
+          window.location.href == 'https://www.mathem.se/' ||
           window.location.href.indexOf("varor") > -1 ||
           window.location.href.indexOf("deli") > -1 ||
           window.location.href.indexOf("inspiration") > -1 ||
           window.location.href.indexOf("/sok") > -1
       ) {
-        setInterval(function(){ Mathem.blockProducts(); }, 1000);
+        setInterval(function(){ Mathem.BlockProducts(); }, 1000);
       }
+    },
+
+    Pages: function() {
+      $.each(Mathem.Snask(), function(i, val) {
+        if (window.location.href.includes(val)) {
+          Mathem.SoundAlarm();
+          return false;
+        }
+      });
+    },
+
+    Cart: function() {
+
     }
 
   }
 
   $(function() {
-    Mathem.init();
+    Mathem.Init();
   });
 
 }(jQuery));
